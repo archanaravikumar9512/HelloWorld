cc5db6cdda5f4ee126f3a6250a7c4c165dab0f66
Archana Birur Ravindra Kumar <archana.birur.ravindra.kumar@ibm.com>
2022-07-06 16:08:32 +0530

    Initial commit

---
+++ src/com/example/Main.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello world");
+        for (int i = 0; i < 100; i++) {
+            System.out.println("");
+        }
+    }
+}
